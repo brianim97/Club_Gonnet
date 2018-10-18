11daf9b61175fb57730ff53d0cd31e8d58640212
Jorge l Gimenez Punta <patito.cuak@gmail.com>
2018-10-18 12:34:35 -0300

    modificacion vista cobro

--- Presentacion/frm_Personas/frm_Persona_Cobro.Designer.cs
+++ Presentacion/frm_Personas/frm_Persona_Cobro.Designer.cs
@@ -28,12 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dtp_Año = new System.Windows.Forms.DateTimePicker();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_Enero = new System.Windows.Forms.Button();
+            this.btn_Febrero = new System.Windows.Forms.Button();
+            this.btn_Marzo = new System.Windows.Forms.Button();
+            this.btn_Abril = new System.Windows.Forms.Button();
+            this.btn_Mayo = new System.Windows.Forms.Button();
+            this.btn_Junio = new System.Windows.Forms.Button();
+            this.btn_Julio = new System.Windows.Forms.Button();
+            this.btn_Agosto = new System.Windows.Forms.Button();
+            this.btn_Septiembre = new System.Windows.Forms.Button();
+            this.btn_Octubre = new System.Windows.Forms.Button();
+            this.btn_Noviembre = new System.Windows.Forms.Button();
+            this.btn_Diciembre = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dtp_Año, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dtp_Año
+            // 
+            this.dtp_Año.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtp_Año.CustomFormat = "yyyy";
+            this.dtp_Año.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtp_Año.Location = new System.Drawing.Point(133, 35);
+            this.dtp_Año.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dtp_Año.Name = "dtp_Año";
+            this.dtp_Año.ShowUpDown = true;
+            this.dtp_Año.Size = new System.Drawing.Size(124, 23);
+            this.dtp_Año.TabIndex = 0;
+            this.dtp_Año.Value = new System.DateTime(2018, 10, 18, 0, 0, 0, 0);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 3);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Enero);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Febrero);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Marzo);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Abril);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Mayo);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Junio);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 96);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(384, 87);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel2, 3);
+            this.flowLayoutPanel2.Controls.Add(this.btn_Julio);
+            this.flowLayoutPanel2.Controls.Add(this.btn_Agosto);
+            this.flowLayoutPanel2.Controls.Add(this.btn_Septiembre);
+            this.flowLayoutPanel2.Controls.Add(this.btn_Octubre);
+            this.flowLayoutPanel2.Controls.Add(this.btn_Noviembre);
+            this.flowLayoutPanel2.Controls.Add(this.btn_Diciembre);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 189);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(384, 87);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // btn_Enero
+            // 
+            this.btn_Enero.Location = new System.Drawing.Point(3, 3);
+            this.btn_Enero.Name = "btn_Enero";
+            this.btn_Enero.Size = new System.Drawing.Size(120, 35);
+            this.btn_Enero.TabIndex = 0;
+            this.btn_Enero.Text = "Enero";
+            this.btn_Enero.UseVisualStyleBackColor = true;
+            // 
+            // btn_Febrero
+            // 
+            this.btn_Febrero.Location = new System.Drawing.Point(129, 3);
+            this.btn_Febrero.Name = "btn_Febrero";
+            this.btn_Febrero.Size = new System.Drawing.Size(120, 35);
+            this.btn_Febrero.TabIndex = 1;
+            this.btn_Febrero.Text = "Febrero";
+            this.btn_Febrero.UseVisualStyleBackColor = true;
+            // 
+            // btn_Marzo
+            // 
+            this.btn_Marzo.Location = new System.Drawing.Point(255, 3);
+            this.btn_Marzo.Name = "btn_Marzo";
+            this.btn_Marzo.Size = new System.Drawing.Size(120, 35);
+            this.btn_Marzo.TabIndex = 2;
+            this.btn_Marzo.Text = "Marzo";
+            this.btn_Marzo.UseVisualStyleBackColor = true;
+            // 
+            // btn_Abril
+            // 
+            this.btn_Abril.Location = new System.Drawing.Point(3, 44);
+            this.btn_Abril.Name = "btn_Abril";
+            this.btn_Abril.Size = new System.Drawing.Size(120, 35);
+            this.btn_Abril.TabIndex = 3;
+            this.btn_Abril.Text = "Abril";
+            this.btn_Abril.UseVisualStyleBackColor = true;
+            // 
+            // btn_Mayo
+            // 
+            this.btn_Mayo.Location = new System.Drawing.Point(129, 44);
+            this.btn_Mayo.Name = "btn_Mayo";
+            this.btn_Mayo.Size = new System.Drawing.Size(120, 35);
+            this.btn_Mayo.TabIndex = 4;
+            this.btn_Mayo.Text = "Mayo";
+            this.btn_Mayo.UseVisualStyleBackColor = true;
+            // 
+            // btn_Junio
+            // 
+            this.btn_Junio.Location = new System.Drawing.Point(255, 44);
+            this.btn_Junio.Name = "btn_Junio";
+            this.btn_Junio.Size = new System.Drawing.Size(120, 35);
+            this.btn_Junio.TabIndex = 5;
+            this.btn_Junio.Text = "Junio";
+            this.btn_Junio.UseVisualStyleBackColor = true;
+            // 
+            // btn_Julio
+            // 
+            this.btn_Julio.Location = new System.Drawing.Point(3, 3);
+            this.btn_Julio.Name = "btn_Julio";
+            this.btn_Julio.Size = new System.Drawing.Size(120, 35);
+            this.btn_Julio.TabIndex = 6;
+            this.btn_Julio.Text = "Julio";
+            this.btn_Julio.UseVisualStyleBackColor = true;
+            // 
+            // btn_Agosto
+            // 
+            this.btn_Agosto.Location = new System.Drawing.Point(129, 3);
+            this.btn_Agosto.Name = "btn_Agosto";
+            this.btn_Agosto.Size = new System.Drawing.Size(120, 35);
+            this.btn_Agosto.TabIndex = 7;
+            this.btn_Agosto.Text = "Agosto";
+            this.btn_Agosto.UseVisualStyleBackColor = true;
+            // 
+            // btn_Septiembre
+            // 
+            this.btn_Septiembre.Location = new System.Drawing.Point(255, 3);
+            this.btn_Septiembre.Name = "btn_Septiembre";
+            this.btn_Septiembre.Size = new System.Drawing.Size(120, 35);
+            this.btn_Septiembre.TabIndex = 8;
+            this.btn_Septiembre.Text = "Septiembre";
+            this.btn_Septiembre.UseVisualStyleBackColor = true;
+            // 
+            // btn_Octubre
+            // 
+            this.btn_Octubre.Location = new System.Drawing.Point(3, 44);
+            this.btn_Octubre.Name = "btn_Octubre";
+            this.btn_Octubre.Size = new System.Drawing.Size(120, 35);
+            this.btn_Octubre.TabIndex = 9;
+            this.btn_Octubre.Text = "Octubre";
+            this.btn_Octubre.UseVisualStyleBackColor = true;
+            // 
+            // btn_Noviembre
+            // 
+            this.btn_Noviembre.Location = new System.Drawing.Point(129, 44);
+            this.btn_Noviembre.Name = "btn_Noviembre";
+            this.btn_Noviembre.Size = new System.Drawing.Size(120, 35);
+            this.btn_Noviembre.TabIndex = 10;
+            this.btn_Noviembre.Text = "Noviembre";
+            this.btn_Noviembre.UseVisualStyleBackColor = true;
+            // 
+            // btn_Diciembre
+            // 
+            this.btn_Diciembre.Location = new System.Drawing.Point(255, 44);
+            this.btn_Diciembre.Name = "btn_Diciembre";
+            this.btn_Diciembre.Size = new System.Drawing.Size(120, 35);
+            this.btn_Diciembre.TabIndex = 11;
+            this.btn_Diciembre.Text = "Diciembre";
+            this.btn_Diciembre.UseVisualStyleBackColor = true;
+            // 
+            // frm_Persona_Cobro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frm_Persona_Cobro";
             this.Text = "frm_Persona_Cobro";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DateTimePicker dtp_Año;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button btn_Julio;
+        private System.Windows.Forms.Button btn_Agosto;
+        private System.Windows.Forms.Button btn_Septiembre;
+        private System.Windows.Forms.Button btn_Octubre;
+        private System.Windows.Forms.Button btn_Noviembre;
+        private System.Windows.Forms.Button btn_Diciembre;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btn_Enero;
+        private System.Windows.Forms.Button btn_Febrero;
+        private System.Windows.Forms.Button btn_Marzo;
+        private System.Windows.Forms.Button btn_Abril;
+        private System.Windows.Forms.Button btn_Mayo;
+        private System.Windows.Forms.Button btn_Junio;
     }
 }
